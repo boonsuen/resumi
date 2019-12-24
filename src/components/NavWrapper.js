26753fca4a6128582f66078388bca327e41d5b63
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-12-24 15:49:35 +0800

    NavWrapper styling progress

--- src/components/NavWrapper.js
+++ src/components/NavWrapper.js
@@ -1,37 +1,27 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import img_logo from '../assets/img/logo.svg';
 
+const StyledNavWrapper = styled.div`
+  background: #e4e0ff;
+  marginBottom: 1.45rem;
+`;
+
 const NavWrapper = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+  <StyledNavWrapper className="container">
+    <Link
+      to="/"
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        color: `white`,
+        textDecoration: `none`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-          <img src={img_logo} />
-        </Link>
-      </h1>
-    </div>
-  </header>
+      <img src={img_logo} alt={siteTitle}/>
+    </Link>
+  </StyledNavWrapper>
 );
 
 NavWrapper.propTypes = {
