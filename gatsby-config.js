9fec9e0f27ff1327a6e9fa26e2c1d600e9247b5a
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-12-24 15:38:53 +0800

    Enable styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   pathPrefix: `/resumi`,
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
