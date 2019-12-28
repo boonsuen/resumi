2da98a5843585c34c5115c0253fb93153487e0b8
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-12-28 22:45:02 +0800

    Implement regular font

--- src/components/GlobalStyle.css.js
+++ src/components/GlobalStyle.css.js
@@ -1,12 +1,26 @@
 import { createGlobalStyle } from 'styled-components';
 
+import GraphikRegularWoff2 from '../assets/fonts/Graphik-Regular-Web.woff2';
+import GraphikMediumWoff2 from '../assets/fonts/Graphik-Medium-Web.woff2';
+
 const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: "Graphik";
+    src: url(${GraphikRegularWoff2}) format('woff2');    
+    font-weight: 400;
+  }
+
+  ${'' /* @font-face {
+    font-family: "Graphik";
+    src: url(${AvenirNextMediumSubsetWoff2}) format('woff2');    font-weight: 500;
+  } */}
+
   html, #___gatsby {
     height: 100%;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif;
+    font-family: "Graphik", -apple-system, BlinkMacSystemFont, Roboto, Helvetica, sans-serif;
     font-weight: 400;
     margin: 0;
     padding: 0;
