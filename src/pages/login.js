8a61ec109674d6d78ae52af95036b110f18f7ae0
Boonsuen Oh <ohboonsuen123@gmail.com>
2019-12-31 11:40:15 +0800

    Login & signup pages

--- src/pages/login.js
+++ src/pages/login.js
@@ -6,48 +6,61 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import img_shieldlock from '../assets/img/shieldlock.svg';
 
-const MainLoginWrapper = styled.div`
+export const MainAuthWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
   margin: 100px 0;
 `;
 
-const ImgWrapper = styled.div`
+export const ImgWrapper = styled.div`
   display: flex;
   justify-content: center;
   flex-grow: 1;
 `;
 
-const LoginContainer = styled.div`
+export const AuthContainer = styled.div`
   width: 600px;
   padding: 55px 75px;
   border-radius: 10px;
-  box-shadow: 0 2px 11px rgba(128, 155, 208, 0.5);
+  box-shadow: 0 2px 11px -2px rgba(128, 155, 208, .5);
 
   h1 {
     color: #000948;
     font-size: 22px;
   }
+
+  form {
+    margin: 0;
+  }
 `;
 
-const AuthFormLabel = styled.label`
+export const AuthFormLabel = styled.label`
+  display: block;
   color: #000948;
+  padding-bottom: 9px;
 `;
 
-const AuthFormInput = styled.input`
+export const AuthFormInput = styled.input`
   display: block;
   width: 100%;
   height: 48px;
   padding-left: 15px;
+  margin-bottom: 24px;
   border: 1px solid #e0e5e9;
   border-radius: 5px;
+  color: #26282D;
   font-size: 16px;
   transition: all .3s;
+  overflow: auto;
 
   &:focus {
     border-color: #0555FF;
     outline: none;
+  }
+
+  &::placeholder {
+    color: #c4c4c3;
   }
 
   &:-webkit-autofill,
@@ -55,11 +68,28 @@ const AuthFormInput = styled.input`
   &:-webkit-autofill:focus,
   &:-webkit-autofill:active {
     border: 1px solid #e0e5e9;
+    -webkit-text-fill-color: #26282D;
     transition: background-color 5000s ease-in-out 0s;
   }
 `;
 
-const AuthFormButton = styled.button`
+const DivSpaceBetween = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ForgotPassword = styled(Link)`
+  color: #6B707B;
+  font-size: 16px;
+  margin-bottom: 7px;
+`;
+
+export const AuthFormActions = styled(DivSpaceBetween)`
+  margin-top: 35px;
+`;
+
+export const AuthFormButton = styled.button`
   width: 112px;
   height: 50px;
   border-radius: 5px;
@@ -68,13 +98,18 @@ const AuthFormButton = styled.button`
   font-size: 18px;
 `;
 
+export const ReverseAuthLink = styled(Link)`
+  color: #26282D;
+  font-size: 16px;
+`;
+
 const LoginPage = () => {
   return (
     <Layout>
       <SEO title="Login" />
-      <MainLoginWrapper>
+      <MainAuthWrapper>
         <ImgWrapper><img src={img_shieldlock} alt="Shield lock" /></ImgWrapper>
-        <LoginContainer>
+        <AuthContainer>
           <h1>Login to your Resumi account</h1>
           <form>
             <AuthFormLabel htmlFor="email">Email</AuthFormLabel>
@@ -82,17 +117,21 @@ const LoginPage = () => {
               type="email" placeholder="you@example.com" 
               spellCheck="false" id="email"
             />
-            <AuthFormLabel htmlFor="password">Password</AuthFormLabel>
+            <DivSpaceBetween>
+              <AuthFormLabel htmlFor="password">Password</AuthFormLabel>
+              <ForgotPassword to="/amnesia">Forgot password?</ForgotPassword>
+            </DivSpaceBetween>
             <AuthFormInput 
               type="password" placeholder="Enter your password" 
               id="password"
             />
-            <Link to="/amnesia">Forgot password?</Link>
-            <AuthFormButton type="submit">Login</AuthFormButton>
-            <Link to="/signup">Don't have an account?</Link>
+            <AuthFormActions>
+              <AuthFormButton type="submit">Log In</AuthFormButton>
+              <ReverseAuthLink to="/signup">Don't have an account?</ReverseAuthLink>
+            </AuthFormActions>
           </form>
-        </LoginContainer>
-      </MainLoginWrapper>
+        </AuthContainer>
+      </MainAuthWrapper>
     </Layout>
   );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,50 @@
 import React from 'react';
 
 import Layout from '../components/layout';
+import SEO from '../components/seo';
+import img_shieldlock from '../assets/img/shieldlock.svg';
+import {
+  MainAuthWrapper,
+  ImgWrapper,
+  AuthContainer,
+  AuthFormLabel,
+  AuthFormInput,
+  AuthFormActions,
+  AuthFormButton,
+  ReverseAuthLink
+} from './login';
 
 const SignupPage = () => {
   return (
     <Layout>
-      <h1>Signup page</h1>
+      <SEO title="Signup" />
+      <MainAuthWrapper>
+        <ImgWrapper><img src={img_shieldlock} alt="Shield lock" /></ImgWrapper>
+        <AuthContainer>
+          <h1>Create your free account</h1>
+          <form>
+            <AuthFormLabel htmlFor="email">Email</AuthFormLabel>
+            <AuthFormInput 
+              type="email" placeholder="you@example.com" 
+              spellCheck="false" id="email"
+            />
+            <AuthFormLabel htmlFor="password">Password</AuthFormLabel>
+            <AuthFormInput 
+              type="password" placeholder="Minimum 6 characters required" 
+              id="password"
+            />
+            <AuthFormLabel htmlFor="confirmPassword">Confirm password</AuthFormLabel>
+            <AuthFormInput 
+              type="password" placeholder="Enter your password again" 
+              id="confirmPassword"
+            />
+            <AuthFormActions>
+              <AuthFormButton type="submit">Sign Up</AuthFormButton>
+              <ReverseAuthLink to="/login">Already have an account?</ReverseAuthLink>
+            </AuthFormActions>
+          </form>
+        </AuthContainer>
+      </MainAuthWrapper>
     </Layout>
   );
 }
