01545ea1f490d1901a91a613a0a7c9ecac076a44
Boonsuen Oh <ohboonsuen123@gmail.com>
2020-01-04 17:49:08 +0800

    Rename app.js to App.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions;
-  const App = path.resolve(`src/components/app.js`)
+  const App = path.resolve(`src/components/App.js`)
   createPage({
     path: 'app',
     component: App,
