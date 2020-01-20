25e9a8310803803157ca0dbd898dbeeac3ff586c
Boonsuen Oh <ohboonsuen123@gmail.com>
2020-01-20 18:00:57 +0800

    Setup loading (fake)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Router, Location } from '@reach/router';
 import { withPrefix } from 'gatsby';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
 import SEO from './seo';
 import Header from './Header';
 import EmptyResume from './EmptyResume';
 import Account from './Account';
 import Editor from './Editor';
+
+const initialLoaderAnimation = keyframes`
+  0% {
+    -webkit-transform: scale(1);
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  100% {
+    -webkit-transform: scale(0.95);
+    transform: scale(0.95);
+    opacity: 0.75;
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  height: 100%;
+
+  #init-loading-svg {
+    width: 44px;
+    animation: ${initialLoaderAnimation} 0.8s cubic-bezier(0.53, 0.22, 0.85, 0.58) 0s infinite alternate;
+  }
+`;
 
 const MainContainer = styled.div`
   display: flex;
@@ -24,30 +47,31 @@ const StyledRouter = styled(Router)`
   height: 100%;
 `;
 
-// const App = () => {
-//   return (
-//     <React.Fragment>
-//       <SEO title="Dashboard" />
-//       <MainContainer>
-//         <Location>
-//           {({ location }) => {
-//             return (
-//               <React.Fragment>
-//                 <Header pathname={location.pathname} />
-//                 <Section>
-//                   <StyledRouter>
-//                     <EmptyResume path={withPrefix("app")} />
-//                     <Account path={withPrefix("app/account")} />
-//                   </StyledRouter>
-//                 </Section>
-//               </React.Fragment>
-//             );
-//           }}
-//         </Location>
-//       </MainContainer>
-//     </React.Fragment>
-//   );
-// };
+const InitialLoading = () => {
+  useEffect(() => {
+    
+  }, []);
+
+  return (
+    <React.Fragment>
+      <SEO title="Loading..." />
+      <LoaderWrapper className="flex-center">
+        <svg id="init-loading-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115 134">
+          <title>Asset 4</title>
+          <g id="Layer_2" data-name="Layer 2">
+            <g id="Layer_1-2" data-name="Layer 1">
+              <g id="PictorialMark">
+                <path d="M110.33,0H4.66C2.09,0,0,1.73,0,3.85V35L39.9,70.44,113.7,6.51C116.53,4.06,114.43,0,110.33,0Z" style={{fill: "#4bc4ff"}} />
+                <path d="M0,35v95a4.13,4.13,0,0,0,4.25,4h96.5a4,4,0,0,0,2.91-6.91Z" style={{fill: "#026dfe"}}/>
+                <polygon points="0 105 39.9 70.44 0 35 0 105" style={{fill: "#1194fe"}} />
+              </g>
+            </g>
+          </g>
+        </svg>
+      </LoaderWrapper>
+    </React.Fragment>
+  );
+};
 
 const Dashboard = () => {
   return (
@@ -74,11 +98,19 @@ const Dashboard = () => {
   );
 };
 
-const App = () => (
-  <StyledRouter>
-    <Dashboard path={withPrefix("/app/*")} />
-    <Editor path={withPrefix("/app/editor")} />
-  </StyledRouter>
-);
+const App = () => {
+  const [ loaded, setLoaded ] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 2000);
+  }, []);
+  return loaded 
+  ? <StyledRouter>
+      <Dashboard path={withPrefix("/app/*")} />
+      <Editor path={withPrefix("/app/editor")} />
+    </StyledRouter>  
+  : <InitialLoading />
+};
 
 export default App;
