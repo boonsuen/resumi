5bdcb2553cd48058e9e9cc10535dcc01c884bae9
Boonsuen Oh <ohboonsuen123@gmail.com>
2020-02-02 21:37:32 +0800

    Add font preload

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,10 +3,15 @@ const GlobalStyle = require("./src/components/GlobalStyle.css").default;
 const { fontFaceRules } = require("./src/components/GlobalStyle.css");
 const { Helmet } = require("react-helmet");
 
+const GraphikRegularWoff2 = require("./src/assets/fonts/Graphik-Regular-Web.woff2");
+const GraphikMediumWoff2 = require("./src/assets/fonts/Graphik-Medium-Web.woff2");
+
 exports.wrapPageElement = ({ element, props }) => {
   return (
     <React.Fragment>
       <Helmet>
+        <link rel="preload" as="font" href={GraphikRegularWoff2} type="font/woff2" crossOrigin="anonymous" />
+        <link rel="preload" as="font" href={GraphikMediumWoff2} type="font/woff2" crossOrigin="anonymous" />
         <style>{fontFaceRules}</style>
       </Helmet>
       <GlobalStyle />
